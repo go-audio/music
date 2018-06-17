b63a407a946ad450f226726367ee407dd81eb89f
Matt Aimonetti <mattaimonetti@gmail.com>
2018-06-16 22:26:36 -0700

    adding richer scale chords but starting to add 7th chords

--- theory/scale_chords.go
+++ theory/scale_chords.go
@@ -5,3 +5,23 @@ var ScaleChords = map[ScaleName][]string{
 	MajorScale:        []string{"maj", "min", "min", "maj", "maj", "min", "mb5"},
 	MelodicMinorScale: []string{"min", "min", "aug", "maj", "maj", "mb5", "mb5"},
 }
+
+// triad
+// four note chords (7th)
+
+var RichScaleChords = map[ScaleName][][]string{
+	MajorScale: [][]string{
+		{"maj", "Maj7"},
+		{"min", "m7"},
+		{"min", "m7"},
+		{"maj", "Maj7"},
+		{"maj", "7"},
+		{"min", "m7"},
+		{"mb5", "m7b5"},
+	},
+	// TODO:
+	MelodicMinorScale: [][]string{
+		{"min"}, {"min"}, {"aug"}, {"maj"}, {"maj"}, {"mb5"}, {"mb5"},
+		{"m7"}, {"m7b5"}, {"Maj7"}, {"min7"}, {"min7"}, {"Maj7"}, {"Maj7"},
+	},
+}
