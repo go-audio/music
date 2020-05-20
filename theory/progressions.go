0852f0dc84cc59a056905cc217ef00555e2f8e74
youshy <devilyoushy@gmail.com>
2020-05-20 11:43:38 +0100

    Update RomanNumerals progressions

--- theory/progressions.go
+++ theory/progressions.go
@@ -50,26 +50,22 @@ var MinorProgressions = [][]int{
 }
 
 /* RomanNumerals are the roman number representation of the chords based on their mode/scale.
-
 Major chord: I, II, III...
-Minor chord: i, ii, iii...
-Augmented chord: I+, II+, III+...
-Diminished chord: vi°, vii°...
-Half-diminished chord: viiØ7...
-Extended chords: ii7, V9, V13...
-Altered tones or chords: #iv, ii#7
+Minor chord: Im, IIm, IIIm...
+Augmented chord: I(#5), II(#5), III(#5)...
+Diminished chord: VI(b5), VI(b5)...
+Half-diminished chord: VIIm7b5...
+Extended chords: IIm7, Imaj7, V9, V13...
+Altered tones or chords: IIm7b9b11
+
+For now will follow only the modal ("church") scales. No extended chords (root + third + fifth).
 */
 var RomanNumerals = map[ScaleName][]string{
-	MajorScale:           {"I", "ii", "iii", "IV", "V", "vi", "vii°"},
-	DorianScale:          {"i", "ii", "bIII", "IV", "v", "vi°", "bVII"},
-	PhrygianScale:        {"i", "bII", "bIII", "iv", "v°", "bVI", "bvii"},
-	LydianScale:          {"I", "II", "iii", "#iv°", "V", "vi", "vii"},
-	MixolydianScale:      {"I", "ii", "iii°", "IV", "v", "vi", "bVII"},
-	MinorPentatonicScale: {"i", "III", "iv", "v", "VII"},
-	HarmonicMinorScale:   {"i", "ii°", "III+", "iv", "V", "VI", "bvii°"},
-	MelodicMinorScale:    {"i", "ii", "III+", "IV", "V", "bvi°", "bvii°"},
-	NaturalMinorScale:    {"i", "ii°", "III", "iv", "v", "VI", "VII"},
-	MajorPentatonicScale: {"i", "ii", "iii", "V", "vi"},
-	LocrianScale:         {"i°", "bII", "biii", "iv", "bV", "bVI", "bvii"},
-	BluesScale:           {"i°", "bIII", "IV", "bV", "V", "bVII", "I"},
+	Ionian:     {"I", "IIm", "IIIm", "IV", "V", "VIm", "VIIm(b5)"},
+	Dorian:     {"Im", "IIm", "bIII", "IV", "V", "VIm(b5)", "bVII"},
+	Phrygian:   {"Im", "bII", "bIII", "IVm", "VIm(b5)", "bVI", "bVIIm"},
+	Lydian:     {"I", "II", "IIIm", "#IVm(b5)", "V", "VIm", "VIIm"},
+	Mixolydian: {"I", "IIm", "IIIm(b5)", "IV", "Vm", "VIm", "bVII"},
+	Aeolian:    {"Im", "IIm(b5)", "III", "IVm", "Vm", "VI", "VII"},
+	Locrian:    {"Im(b5)", "bII", "bIIIm", "IVm", "bV", "bVI", "bVIIm"},
 }
